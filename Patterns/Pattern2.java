fbc4b6fe1c461b521d849be37cc833e89bbc4e21
1301yashraj <1301yashraj@gmail.com>
2023-10-11 01:48:52 +0530

    Day 2

---
+++ Patterns/Pattern2.java
@@ -0,0 +1,66 @@
+import java.util.*;
+/*T
+    Hollow Triangle and a simple triangle
+          *                    *
+        *   *                * * *
+    Something like this.
+ */
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> inputs = Pattern2.input();
+        switch (inputs.get(1)) {
+            case 1:
+                Pattern2.hollow(inputs.get(0));
+                break;
+            case 2:
+                Pattern2.solid(inputs.get(0));
+        }
+    }
+
+    public static void hollow(int size) {
+        int rows = size, columns = 2 * size - 1; // (size-1) on both side + 1 at centre
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < columns; col++) {
+                // print * at size-row position than row spaces than print *
+                if (((size - row - 1) == col) || (col == (size + row - 1)) || row == (rows - 1))
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void solid(int size) {
+        int rows = size, columns = 2 * size - 1; // (size-1) on both side + 1 at centre
+        for (int row = 0; row < rows; row++) {
+            int count = 0;
+            for (int col = 0; col < columns; col++) {
+                // print * at size-row position than row spaces than print *
+                if (((size - row - 1) == col) || (col == (size + row - 1)) || row == (rows - 1)) {
+                    System.out.print("* ");
+                    count++;
+                }
+
+                else {
+                    if (count == 1 && row != 0)
+                        System.out.print("* ");
+                    else
+                        System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static ArrayList<Integer> input() {
+        ArrayList<Integer> newList = new ArrayList<Integer>();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of triangle ");
+        newList.add(sc.nextInt());
+        System.out.print("\nEnter type of triangle 1 Hollow and 2 Solid");
+        newList.add(sc.nextInt());
+        return newList;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Day_To_Day
-Just some practice Question with DSA ,I try to do daily 
+
+Just some practice Question with DSA ,I try to do daily
+
+# 9th of October 2023
+
+Starting again and lets see how consistent I can be.
+DSA stands Data Structures and Algorithms.
+
+Data structure is the way in which data can be stored in the memory(in RAM),
+each Data Structure has some pros and cons and we select a particular data structure based on the need of our application , having said that there are some data structures which are used more frequently used than others e.g array.
+
+Algorithms are way of solving problem and there are many problems or groups of problems in which a particular algorithm is the best possible solution , some algorithms are easy and can be taught by us and others are difficult and takes a long time to develop and yes we can use them in our applications to get the best performance out of our application.
+
+Data structure generally help us with the space optimization and
+Algorithms helps us with time optimization.
+
+Data structure are predefined by the programming language you are working with
+Algorithms are designed by us and in this algorithm we use are DS as per our need. So Algorithms is what we have more control over and when we talk about DSA
+mostly we talk about.
+
+# 10th of October 2023
+
+Starting with something simple just beacuse I dont want to run away from the task by fear of problems on Day 2.
+So lets do some Patterns 
