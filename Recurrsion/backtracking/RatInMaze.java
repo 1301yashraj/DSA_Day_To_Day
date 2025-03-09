b848829bbf1e5d2af8d17e9db15c3dca0d0bc4ff
1301yashraj <1301yashraj@gmail.com>
2025-03-10 00:16:04 +0530

    correction in return statements

--- Recurrsion/backtracking/RatInMaze.java
+++ Recurrsion/backtracking/RatInMaze.java
@@ -14,30 +14,26 @@ public class RatInMaze {
     }
 
     private static void findPath(int[][] maze, int row, int col, StringBuilder ans, boolean[][] visited) {
-        // System.out.println("Row >>" + row + " col >> " + col);
-        if (row >= 0 && col >= 0 && row < maze.length && col < maze[0].length) {
-            if (row == maze.length - 1 && col == maze[0].length - 1)
-                System.out.println(ans.toString());
-        } else {
-
+        if (row == maze.length - 1 && col == maze[0].length - 1) {
+            System.out.println(ans.toString());
             return;
         }
+        if (row < 0 || col < 0 || row >= maze.length || col >= maze[0].length)
+            return;
 
-        int atPoint = maze[row][col];
-        if (atPoint == 0 || visited[row][col])
+        if (maze[row][col] == 0 || visited[row][col])
             return;
 
         visited[row][col] = true;
-        // go down
         findPath(maze, row + 1, col, ans.append("D"), visited);
         ans.deleteCharAt(ans.length() - 1);
-        // go right
+
         findPath(maze, row, col + 1, ans.append("R"), visited);
         ans.deleteCharAt(ans.length() - 1);
-        // go up
+
         findPath(maze, row - 1, col, ans.append("U"), visited);
         ans.deleteCharAt(ans.length() - 1);
-        // go left
+
         findPath(maze, row, col - 1, ans.append("L"), visited);
         ans.deleteCharAt(ans.length() - 1);
 
